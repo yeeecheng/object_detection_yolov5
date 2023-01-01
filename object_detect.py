608d631dbf1add906a46f28ec4467d99c23747b2
yeeecheng <yichengliao1003@gmail.com>
2023-01-02 00:49:34 +0800

    add detect label

--- object_detect.py
+++ object_detect.py
@@ -10,7 +10,7 @@ def detect(opt):
     
     yolov5_path = os.path.join(opt.root,"./yolov5")
     weight_path = opt.weight
-    
+    imgsz = opt.imgsz
     model = torch.hub.load(yolov5_path, 'custom', weight_path, source='local' )
     
     cap = cv2.VideoCapture(0)
@@ -25,25 +25,28 @@ def detect(opt):
             print("Cannot receive Frame")
         
         img_path = "./detect.png"
-        frame = cv2.resize(frame,(320,320))
+        frame = cv2.resize(frame,(imgsz,imgsz))
         cv2.imwrite(img_path,frame)
         # IoU門檻值
         model.iou = opt.iou
         # 信心門檻值
         model.conf = opt.conf
         # 辨識
-        results =model(img_path,size=(320,320))
+        results =model(img_path,size=(imgsz,imgsz))
         # Results
         results.print()  
         results.xyxy[0]  
        
         # 轉化成json
         results =results.pandas().xyxy[0].to_json()
-        results =json.loads(results)
-            
-        # # 存json
+        results =json.loads(results)["name"]
+        
+        write_data=""
+        for i in results:
+            write_data+=results[i]
+        ## 存txt
         with open(opt.save,"w") as f:
-            json.dump(results,f,ensure_ascii=False)
+            f.write(write_data)
         
 
 def main(opt):
@@ -55,9 +58,10 @@ def parse_opt(known=False):
     parser =argparse.ArgumentParser()
     parser.add_argument("--root",type=str , default=ROOT )
     parser.add_argument("--weight",type=str , default=os.path.join(ROOT,"./best-int8_edgetpu.tflite") )
+    parser.add_argument("--imgsz",type=int , default=320)
     parser.add_argument("--iou",type=float,default=0.3)
     parser.add_argument("--conf",type=float,default=0.5)
-    parser.add_argument("--save",type=str ,default=os.path.join(ROOT,'./detect_result.json'))
+    parser.add_argument("--save",type=str ,default=os.path.join(ROOT,'./detect_result.txt'))
     return parser.parse_args()[0] if known else parser.parse_args()
 
 
@@ -73,5 +77,3 @@ if __name__ == "__main__":
     opt = parse_opt()
     main(opt)
 
-
-#subprocess.run("python3 ./yolov5/detect.py --weight ./best-int8_edgetpu.tflite --data ./dataset.yaml --imgsz 320 --iou-thres 0.3 --conf-thres 0.6 --source 0".split(" "))
